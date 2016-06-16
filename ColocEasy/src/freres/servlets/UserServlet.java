47cc94f540f22f30c6d94d7b2c5ff7171264d7e4
younessadmi <younes.sadmi@gmail.com>
2016-06-17 00:20:38 +0200

    code smells are a pain

--- ColocEasy/src/freres/servlets/UserServlet.java
+++ ColocEasy/src/freres/servlets/UserServlet.java
@@ -23,13 +23,13 @@ public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String USER_SESSION = "userSession";
 
-	private final String LOGIN = "login"; 
-	private final String PWD = "password"; 
-	private final String NEWPWD = "password_new"; 
-	private final String NOMATCH = "The passwords do not match."; 
-	private final String ERRORMESSAGE = "error_message"; 
-	private final String ACTION = "action"; 
-	private final String SUCCESS = "success"; 
+	private static final String LOGIN = "login"; 
+	private static final String PWD = "password"; 
+	private static final String NEWPWD = "password_new"; 
+	private static final String NOMATCH = "The passwords do not match."; 
+	private static final String ERRORMESSAGE = "error_message"; 
+	private static final String ACTION = "action"; 
+	private static final String SUCCESS = "success"; 
 	
 	private IUserManager userManager = new UserManagerDB();
 
@@ -68,27 +68,27 @@ public class UserServlet extends HttpServlet {
 	}
 
 	private void login(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		final String login = request.getParameter(this.LOGIN);
-		final String password = request.getParameter(this.PWD);
+		final String login = request.getParameter(UserServlet.LOGIN);
+		final String password = request.getParameter(UserServlet.PWD);
 
 		if (login == null || password == null) {
 			System.out.println("Navigating to login.");
 		} else if (this.userManager.checkLoginWithPassword(login, password)) {
-				request.getSession().setAttribute(this.USER_SESSION, this.userManager.getUser(login));
-				System.out.println(request.getSession().getAttribute(this.USER_SESSION) + " logging in, redirecting to index.");
+				request.getSession().setAttribute(UserServlet.USER_SESSION, this.userManager.getUser(login));
+				System.out.println(request.getSession().getAttribute(UserServlet.USER_SESSION) + " logging in, redirecting to index.");
 				response.sendRedirect("index");
 				return;
 			}  else {
-			request.setAttribute(this.ERRORMESSAGE, "Login ou mot de passe incorrect");
+			request.setAttribute(UserServlet.ERRORMESSAGE, "Login ou mot de passe incorrect");
 			System.out.println("Login failed");
 		}
-		request.setAttribute(this.ACTION, this.LOGIN);
+		request.setAttribute(UserServlet.ACTION, UserServlet.LOGIN);
 		request.getRequestDispatcher("/WEB-INF/html/login.jsp").forward(request, response);
 	}
 
 	private void create(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		 final String login = request.getParameter(this.LOGIN);
-		 final String password = request.getParameter(this.PWD);
+		 final String login = request.getParameter(UserServlet.LOGIN);
+		 final String password = request.getParameter(UserServlet.PWD);
 		 final String confirm = request.getParameter("confirm");
 		 final String lastName = request.getParameter("last_name");
 		 final String firstName = request.getParameter("first_name");
@@ -98,26 +98,26 @@ public class UserServlet extends HttpServlet {
 		 if (login != null && password != null) {
 			 if(password.equals(confirm)){
 				 if (this.userManager.checkLogin(login)) {
-					 request.setAttribute(this.ERRORMESSAGE, "User already exists. Please chose another");
-					 System.out.println(request.getAttribute(this.ERRORMESSAGE));
+					 request.setAttribute(UserServlet.ERRORMESSAGE, "User already exists. Please chose another");
+					 System.out.println(request.getAttribute(UserServlet.ERRORMESSAGE));
 				 } else {
 					 this.userManager.createUser(login, password, lastName, firstName, birthDate, sexe);
-					 request.setAttribute(this.SUCCESS, "User succesfully created");
-					 System.out.println(request.getAttribute(this.SUCCESS));
+					 request.setAttribute(UserServlet.SUCCESS, "User succesfully created");
+					 System.out.println(request.getAttribute(UserServlet.SUCCESS));
 					 response.sendRedirect("confirm");
 					 return;
 				 }
 			 }
-			 request.setAttribute(this.ERRORMESSAGE, this.NOMATCH);
-			 System.out.println(this.NOMATCH);
+			 request.setAttribute(UserServlet.ERRORMESSAGE, UserServlet.NOMATCH);
+			 System.out.println(UserServlet.NOMATCH);
 		 }
-		 request.setAttribute(this.ACTION, "create");
+		 request.setAttribute(UserServlet.ACTION, "create");
 		 request.getRequestDispatcher("/WEB-INF/html/signup.jsp").forward(request, response);
 	 }
 	 
 	private void profile(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException { 
-		if(request.getSession().getAttribute(this.USER_SESSION) != null) {
-			User u = this.userManager.getUser(request.getSession().getAttribute(this.USER_SESSION).toString());
+		if(request.getSession().getAttribute(UserServlet.USER_SESSION) != null) {
+			User u = this.userManager.getUser(request.getSession().getAttribute(UserServlet.USER_SESSION).toString());
 			request.setAttribute("user", u);
 
 			final String birthDate = request.getParameter("birthday_submit");
@@ -126,30 +126,30 @@ public class UserServlet extends HttpServlet {
 			final String lastName = request.getParameter("last_name");
 			final String id = request.getParameter("id");
 			final String sexe = request.getParameter("sexe");
-			final String password = request.getParameter(this.PWD);
-			final String confirm = request.getParameter("password_confirmation") != "" ? request.getParameter(this.NEWPWD): u.getPassword();
-			final String newPass = request.getParameter(this.NEWPWD) != "" ? request.getParameter(this.NEWPWD): u.getPassword();
+			final String password = request.getParameter(UserServlet.PWD);
+			final String confirm = request.getParameter("password_confirmation") != "" ? request.getParameter(UserServlet.NEWPWD): u.getPassword();
+			final String newPass = request.getParameter(UserServlet.NEWPWD) != "" ? request.getParameter(UserServlet.NEWPWD): u.getPassword();
 			
 			if(id != null) {
-				if(newPass.equals(confirm) && password.equals(((User)request.getSession().getAttribute(this.USER_SESSION)).getPassword())){
+				if(newPass.equals(confirm) && password.equals(((User)request.getSession().getAttribute(UserServlet.USER_SESSION)).getPassword())){
 					if(this.userManager.editUser(id, login, newPass, lastName, firstName, birthDate, sexe)){
-						request.getSession().setAttribute(this.USER_SESSION, this.userManager.getUser(Integer.parseInt(id)));
-						request.setAttribute(this.SUCCESS, "The user "+login+" has been updated.");
+						request.getSession().setAttribute(UserServlet.USER_SESSION, this.userManager.getUser(Integer.parseInt(id)));
+						request.setAttribute(UserServlet.SUCCESS, "The user "+login+" has been updated.");
 						System.out.println("The user "+login+" has been updated.");
 					}
 				} else {
-					 request.setAttribute(this.ERRORMESSAGE, this.NOMATCH);
-					 System.out.println(this.NOMATCH);
+					 request.setAttribute(UserServlet.ERRORMESSAGE, UserServlet.NOMATCH);
+					 System.out.println(UserServlet.NOMATCH);
 				}
 			}
 		}
-		request.setAttribute(this.ACTION, "profile");
+		request.setAttribute(UserServlet.ACTION, "profile");
 		request.getRequestDispatcher("/WEB-INF/html/profile.jsp").forward(request, response);
 	}
 	 
 	private void logout(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		System.out.println(request.getSession().getAttribute(USER_SESSION)+" logging out, redirecting to index");
-		request.getSession().removeAttribute(this.USER_SESSION);
+		request.getSession().removeAttribute(UserServlet.USER_SESSION);
 		response.sendRedirect("index");
 	}
 
